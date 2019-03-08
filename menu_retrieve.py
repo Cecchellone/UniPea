13eb1a6cb811cdcc4fd57b2ec3cc2879884a43c1
cecchellone <cecchellone@gmail.com>
2019-03-08 12:31:33 +0100

    comprehensibility update
    Added temporary mensa selector

--- menu_retrieve.py
+++ menu_retrieve.py
@@ -5,20 +5,28 @@ import datetime
 import urllib.request
 from pdf2image import convert_from_path as cfp
    
+def getperiod(**kwargs):
+    period = "weekly"
+    for arg, val in kwargs.items():
+        if arg == "period":
+            period = val
+
+    Today = datetime.datetime.now()
+    if period == "weekly":
+        Lastmon = Today - datetime.timedelta(days=Today.weekday())
+        Nextsun = Lastmon + datetime.timedelta(days=6)
+        return tuple([Lastmon, Nextsun])
+    return ValueError
+
 def geturl(date, name):
     base_url = "http://www.dsu.toscana.it/it/Men%C3%B9"
-    
-    Today = datetime.datetime.now()
-    Lastmon = Today - datetime.timedelta(days=Today.weekday())
-    Nextsun = Lastmon + datetime.timedelta(days=6)
-    
-    PM = Lastmon.strftime("%d.%m.%y")
-    PS = Nextsun.strftime("%d.%m.%y")
-    return base_url + "-dal-" + PM + "-al-" + PS + "-" + name + ".pdf"
+    begin, end = getperiod()
+
+    return base_url + "-dal-" + begin.strftime("%d.%m.%y") + "-al-" + end.strftime("%d.%m.%y") + "-" + name + ".pdf"
 
 async def retrieve_file(url, retry_attempts, retry_interval):
     for i in range(retry_attempts):
-        print("attempt:", i+1, "of", retry_attempts)
+        print("attempt", i+1, "of", retry_attempts)
         try:
             file_name, headers = urllib.request.urlretrieve(url)
             print("Menù retrieved")
@@ -30,7 +38,19 @@ async def retrieve_file(url, retry_attempts, retry_interval):
         print("Unable to locate the file")
         return ""
 
+async def save_png(pages, path, name):
+    page_num = len(pages)
+
+    for i, page in enumerate(pages):
+        print("Exporting page", i+1, "of", page_num)
+        file_name = name
+        if page_num >1:
+            file_name = "{0}_{2}.{1}".format(*os.path.splitext(name) + [i])
+        page.save(os.path.join(path, file_name), "PNG")
+    return page_num
+
 async def makeimg(mensa_name):
+    mensa_name = mensa_name.lower()
     path = os.path.dirname(os.path.realpath(__file__))
     url = geturl(datetime.datetime.now(), mensa_name)
     print("Url:", url)
@@ -38,12 +58,13 @@ async def makeimg(mensa_name):
     pdf_name = await retrieve_file(url, 24, 3600)
 
     if pdf_name != "":
+        print("Converting pages")
         pages = cfp(pdf_name, 500)
-        pages[0].save(path + "/" + mensa_name + ".jpg", "JPEG")
+        await save_png(pages, path, mensa_name + ".png")
         print("Menù exported")
         os.remove(pdf_name)
         print("Cleared temporary files")
     else:
         print("Unable to find the menù")
 
-asyncio.run(makeimg("martiri"))
+asyncio.run(makeimg(input("Type mensa name: ")))
